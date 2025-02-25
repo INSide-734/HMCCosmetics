650cf751680f809d9468c01ea005551af09c13c8
LoJoSho <LoJoShogaming@gmail.com>
2025-02-25 10:45:25 -0600

    chore: improvements to HookBetterHud

--- common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/misc/HookBetterHud.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/misc/HookBetterHud.java
@@ -8,6 +8,7 @@ import kr.toxicity.hud.api.player.HudPlayer;
 import me.lojosho.hibiscuscommons.hooks.Hook;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
@@ -15,10 +16,11 @@ public class HookBetterHud extends Hook {
 
     public HookBetterHud() {
         super("BetterHUD");
+        setActive(true);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerEnterWardrobe(PlayerWardrobeEnterEvent event) {
+    public void onPlayerEnterWardrobe(@NotNull PlayerWardrobeEnterEvent event) {
         if (!Settings.isWardrobeHideHud()) return;
         UUID uuid = event.getUniqueId();
         HudPlayer hudPlayer = BetterHud.getInstance().getPlayerManager().getHudPlayer(uuid);
@@ -27,7 +29,7 @@ public class HookBetterHud extends Hook {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerLeaveWardrobe(PlayerWardrobeLeaveEvent event) {
+    public void onPlayerLeaveWardrobe(@NotNull PlayerWardrobeLeaveEvent event) {
         if (!Settings.isWardrobeHideHud()) return;
         UUID uuid = event.getUniqueId();
         HudPlayer hudPlayer = BetterHud.getInstance().getPlayerManager().getHudPlayer(uuid);
